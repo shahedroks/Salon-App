3bf8d57908ec07bfe608269d4d4916059f6aee4e
shahedroks <shahedroks@gmail.com>
2025-04-08 19:52:02 +0600

    firebase add successfully connect

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:selon/firebase_options.dart';
 import 'package:selon/pages/home_page/users_home_page.dart';
-import 'package:selon/pages/signIn_page/signin_page.dart';
 import 'package:selon/pages/signin_page/state_management/bloc/signin_bloc.dart';
 import 'package:selon/pages/signup_page/signup_page.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -24,8 +22,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         initialRoute: "/",
         routes: {
-          "/": (context) => SignUpPage(),
-          "/users_home_page": (context) => UsersHomePage(),  // users_home_page
+          "/gfhwg": (context) => SignUpPage(),
+          "/": (context) => UsersHomePage(), // users_home_page
         },
       ),
     );
